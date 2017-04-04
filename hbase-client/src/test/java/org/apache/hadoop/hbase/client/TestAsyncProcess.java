3d9310fdf0d9133836347cc862585cd737bacf70
anuphalarnkar <anup_halarnkar@persistent.co.in>
2017-04-04 17:33:30 +0530

    TestAsyncProcess.java -- line 1188 should return -2 as result of compareTo

--- hbase-client/src/test/java/org/apache/hadoop/hbase/client/TestAsyncProcess.java
+++ hbase-client/src/test/java/org/apache/hadoop/hbase/client/TestAsyncProcess.java
@@ -1185,7 +1185,7 @@ public class TestAsyncProcess {
     assertTrue(action_2.equals(action_3));
     assertFalse(action_0.equals(action_3));
     assertEquals(0, action_0.compareTo(action_0));
-    assertEquals(-1, action_0.compareTo(action_1));
+    assertEquals(-2, action_0.compareTo(action_1));
     assertEquals(1, action_1.compareTo(action_0));
     assertEquals(0, action_1.compareTo(action_2));
   }
